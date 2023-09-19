e446bf13fabad3bbe65fdd0ad74be05e86e56cb9
Md. Nadim Hossain <108954901+nadu6969@users.noreply.github.com>
2023-09-20 03:47:53 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========== Typing Animation ========== */
+var typed = new Typed(".typing", {
+    strings:["","Web Designer","And Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
